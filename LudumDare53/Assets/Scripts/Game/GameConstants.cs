2cad2d847bd3c71a6d3cd67ebc08df38b4f7c050
Federico Andrucci <federico.andrucci@gmail.com>
2023-04-28 17:51:20 +0200

    game constants

---
+++ LudumDare53/Assets/Scripts/Game/GameConstants.cs
@@ -0,0 +1,8 @@
+namespace Game {
+    
+    public class GameConstants {
+
+        public const string k_AxisNameVertical = "Vertical";
+        public const string k_AxisNameHorizontal = "Horizontal";
+    }
+}
