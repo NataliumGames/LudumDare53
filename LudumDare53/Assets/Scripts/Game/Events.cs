326ad9fde6a30fd99a1c23e69f8dae2b87e6709f
Federico Andrucci <federico.andrucci@gmail.com>
2023-04-28 17:21:39 +0200

    events utils

---
+++ LudumDare53/Assets/Scripts/Game/Events.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Game.Managers;
+using UnityEngine;
+
+namespace Game {
+
+    public static class Events {
+        public static GameOverEvent GameOverEvent = new GameOverEvent();
+        public static DisplayMessageEvent DisplayMessageEvent = new DisplayMessageEvent();
+    }
+
+    public class GameOverEvent : GameEvent {
+        
+    }
+
+    public class DisplayMessageEvent : GameEvent {
+        
+    }
+}
