65fb936d59899009d338a9e9ef5ec39359f47507
Michele Righi <righi.michy@gmail.com>
2023-04-30 01:02:12 +0200

    update MinigameMiky + SetupMinigameMiky (test)
    + 3d engagement bar -> UI 2d engagement bar (using Image Filling);
    + now scales with screen size;

--- LudumDare53/Assets/Scripts/MinigameMiky/EngagementBar.cs
+++ LudumDare53/Assets/Scripts/MinigameMiky/EngagementBar.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EngagementBar : MonoBehaviour
 {
     public float decrease = 0.02f;
     public float current = 1.0f; // start from max bar
-    public float pointValue = 0.02f;
+    public float pointValue = 0.025f;
+
+    private Image imageBar;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        imageBar = gameObject.GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -22,21 +25,16 @@ public class EngagementBar : MonoBehaviour
             current = 1.0f;
         }
 
-        if (transform.localScale.y > 0.0f)
+        if (current > 0.0f)
         {
-            Vector3 p = transform.localPosition;
-            Vector3 s = transform.localScale;
-
-            /*transform.localPosition = Vector3.Lerp(p, new Vector3(-8f + points / 2, p.y, p.z), 15 * Time.deltaTime);
-            transform.localScale = Vector3.Lerp(s, new Vector3(s.x, points, s.z), 15 * Time.deltaTime);*/
-
             current -= decrease * Time.deltaTime;
             if (current < 0.0f)
                 current = 0.0f;
-
-            transform.localPosition = Vector3.Lerp(p, new Vector3(-8f + current * 8, p.y, p.z), 15 * Time.deltaTime);
-            transform.localScale = Vector3.Lerp(s, new Vector3(s.x, current * 16, s.z), 15 * Time.deltaTime);
         }
+
+        Debug.Log(current);
+
+        imageBar.fillAmount = current;
     }
 
     void AddPoints(float pts)
