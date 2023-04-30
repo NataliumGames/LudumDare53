37affd7a824d5821b3301e2f0cf1da7c53280bb9
Alex Gianelli <56556129+Noesh@users.noreply.github.com>
2023-04-30 11:52:08 +0200

    Update main menu

--- LudumDare53/Assets/Scripts/UI/MainMenuUIManager.cs
+++ LudumDare53/Assets/Scripts/UI/MainMenuUIManager.cs
@@ -8,17 +8,25 @@ namespace UI {
 
         private Button startButton;
         private Button quitButton;
+        private Button infoButton;
 
         private void Awake() {
             startButton = transform.GetChild(0).GetComponent<Button>();
             quitButton = transform.GetChild(1).GetComponent<Button>();
+            infoButton = transform.GetChild(2).GetComponent<Button>();
             
             startButton.onClick.AddListener(OnStartButtonPressed);
             quitButton.onClick.AddListener(OnQuitButtonPressed);
+            infoButton.onClick.AddListener(OnInfoButtonPressed);
         }
+        
 
         private void OnStartButtonPressed() {
             FindObjectOfType<SceneManager>().LoadScene();
+        }
+        
+        private void OnInfoButtonPressed() {
+            // TODO: show overlay with tutorial and info
         }
 
         private void OnQuitButtonPressed() {
