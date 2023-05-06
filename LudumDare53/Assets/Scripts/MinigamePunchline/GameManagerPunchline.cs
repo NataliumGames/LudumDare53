96875fa7087cd182b10bb497369ffa2dedccd985
Michele Righi <righi.michy@gmail.com>
2023-05-06 03:27:34 +0200

    update test scene for punchline
    + edited gradientBar (fixed intervals so it's more precise);
    + changed multiplier threshold for 10x (now >= 4.9f);
    + almost finished GameManagerPunchline (need to fix punches vfx);
    
    TO-DO:
    + fix punches vfx (punches impact works just fine);
    + trophy in UI;
    
    Future development:
    + overall stats;
    + add a purpose for the score (maybe starting point of the bar? Or smth;

--- LudumDare53/Assets/Scripts/MinigamePunchline/GameManagerPunchline.cs
+++ LudumDare53/Assets/Scripts/MinigamePunchline/GameManagerPunchline.cs
@@ -1,8 +1,13 @@
 using Game;
+using Game.Managers;
 using Managers;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
+using System.Linq;
 using UnityEngine;
+using TMPro;
+using UI;
 
 public class GameManagerPunchline : MonoBehaviour
 {
@@ -13,9 +18,17 @@ public class GameManagerPunchline : MonoBehaviour
      * 10/11 might be a great goal to achieve the 100%.
      */ 
     private const float PERFECT_CPS = 11.0f;
+    private const float MAX_SHAKE_AMOUNT = 0.2f;
+    private const int MAX_PARTICLES = 200;
+    private const float MAX_USER_DELAY = 0.25f; // delay after which the shake/vfx stops if the user doesn't press the spacebar
 
     public GameObject controlsGameObject;
     public GameObject gameStatsGameObject;
+    public GameObject gameOverGameObject;
+    public TextMeshProUGUI gameOverDescription;
+    public GameObject multiplierCanvas;
+
+    public ParticleSystem[] particleSystems;
 
     public float time = 20.0f;
 
@@ -24,12 +37,18 @@ public class GameManagerPunchline : MonoBehaviour
     private CameraShake cameraShake;
     private PunchlineUI punchButton;
 
+    private GaugeBar gaugeBar;
+    private TextMeshProUGUI multiplierText;
+
     private int counter = 0;
-    // spacebar component
+    private int counterStep;
 
     private bool gameRunning = false;
+    private bool gameOver = false;
     
     private float perfectScore;
+
+    private float lastPressed = 0.0f; // timestamp representing the last time the user pressed the spacebar
 
     void Start()
     {
@@ -38,27 +57,45 @@ public class GameManagerPunchline : MonoBehaviour
         cameraShake = FindObjectOfType<CameraShake>();
         punchButton = FindObjectOfType<PunchlineUI>();
 
-        perfectScore = PERFECT_CPS * time;
-        timer.timeRemaining = time;
+        gaugeBar = multiplierCanvas.GetComponentInChildren<GaugeBar>();
+        multiplierText = multiplierCanvas.GetComponentInChildren<TextMeshProUGUI>();
 
-        // show
+        perfectScore = PERFECT_CPS * time;
+        counterStep = (int) perfectScore / 5;
+
+        timer.timeRemaining = time;
+        cameraShake.shakeDuration = 0.0f;
+        cameraShake.shakeAmount = 0.0f;
+
+        for(int i = 0; i < particleSystems.Length; i++)
+        {
+            var loop = particleSystems[i].main.loop;
+            var emission = particleSystems[i].emission;
+            var duration = particleSystems[i].main.duration;
+
+            loop = false;
+            emission.rateOverTime = 0.0f;
+            duration = time;
+        }
+
+        // show panels
         controlsGameObject.SetActive(true);
         gameStatsGameObject.SetActive(false);
+        gameOverGameObject.SetActive(false);
     }
-
 
     void Update()
     {
-        if (!gameRunning && Input.GetKeyDown("space"))
+        if (!gameRunning && !gameOver && Input.GetKeyDown("space"))
         {
             StartGame();
         }
 
-        if (gameRunning)
+        if (gameRunning && !gameOver)
         {
             if (Input.GetKeyDown("space"))
             {
-                punchButton.Press();
+                PunchLine();
             }
 
             if (Input.GetKeyUp("space"))
@@ -66,6 +103,62 @@ public class GameManagerPunchline : MonoBehaviour
                 punchButton.Release();
             }
         }
+
+        // BRAINSTORMING
+
+        // praticamente andrebbe qui
+        // camerashake * counter ecc, se è startato
+
+        // salvo timestamp ultima volta chiamato (camera shake)
+
+        // se shake 250ms
+        // se son passati 50ms dall'ultimo, richiamo lo shake
+
+        // dovrei fare delle prove forse perché ho paura che se duri poco poi vada tipo a scatti ripartendo sempre dalla posizione iniziale e
+        // l'effetto sia bruttino
+
+        // allora il camera shake funziona così:
+        // c'è una durata totale e un'intensità:
+        // nella update lui diminuisce in base al deltatime in modo da fare un massimo di tempo pari alla durata complessiva
+
+        // quindi a noi basta fare il toggle della durata (0/quanto vogliamo farlo durare - eventualmente anche il massimo, tipo 20, e quando l'utente
+        // non clicca per un tot di tempo lo disattiviamo, o qualcosa del genere)
+
+        // esatto, dobbiamo almeno tenere una variabile aggiuntiva tipo
+
+        // yees, max 20 secondi
+
+        // intensità 0.0/1.0 toggle
+
+        // aggiorniamo continuamente il timestamp dell'ultima volta che è stato premuto
+
+        // controlliamo se:
+        // il timestamp è all'interno della durata di shake
+        // -> intensità >0 => viene calcolata (come facciamo già in controller)
+        
+        // altrimenti intensità =0
+
+       
+        // funzione da chiamare con argomento tempo (chiamante fa sia camera controller che vfx emitter)
+
+        // ahah sì
+
+        // sì adesso finisco una roba e ci provo (tutto sempre nella scena di test)
+        
+        // il trofeo ancora no, se avete un'idea di dove metterlo possiamo provare a farlo
+
+        if (Time.realtimeSinceStartup - lastPressed >= MAX_USER_DELAY)
+        {
+            cameraShake.shakeAmount = 0.0f;
+            // vfx
+            foreach(ParticleSystem ps in particleSystems)
+            {
+                var emission = ps.emission;
+                emission.rateOverTime = 0.0f;
+            }
+        }
+        
+        // TO-DO
     }
 
     private void StartGame()
@@ -77,10 +170,20 @@ public class GameManagerPunchline : MonoBehaviour
 
         timer.StartTimer();
 
-        cameraShake.shakeDuration = 20.0f;
+        // start shake timer
+        cameraShake.shakeDuration = time;
         cameraShake.shakeAmount = 0.0f;
 
+        // start vfx timer
+        foreach (ParticleSystem ps in particleSystems)
+        {
+            ps.Play();
+        }
+
         // start music
+        FindObjectOfType<AudioManager>().PlayMusic("Punchline");
+
+        EventManager.AddListener<TimerTimeOutEvent>(OnTimerTimeoutEvent);
     }
 
     private void PunchLine()
@@ -89,13 +192,83 @@ public class GameManagerPunchline : MonoBehaviour
         punchButton.Press();
         counter++;
 
-        // Show VFX
+        // Update GaugeBar & Multiplier
+        gaugeBar.SetBarValue(counter / perfectScore);
+        multiplierText.SetText("x" + updateMultiplier());
+
         // Shake Camera
+        cameraShake.shakeAmount = (counter / perfectScore) * MAX_SHAKE_AMOUNT;
+        lastPressed = Time.realtimeSinceStartup;
+
+        // Show VFX
+        foreach (ParticleSystem ps in particleSystems)
+        {
+            var emission = ps.emission;
+            emission.rateOverTime = (counter / perfectScore) * MAX_PARTICLES;
+        }
+
         // Play sound
+        audioManager.PlayPunch();
     }
 
     private void OnTimerTimeoutEvent(TimerTimeOutEvent evt)
     {
+        GameFlowManager gameFlowManager;
+        float average = 0.0f, multiplier = 0.0f, finalScore = 0.0f;
+
+        gameRunning = false;
+        gameOver = true;
         punchButton.Release();
+        punchButton.gameObject.SetActive(false);
+
+        cameraShake.shakeAmount = 0.0f;
+        // terminate vfx
+
+        gameFlowManager = FindObjectOfType<GameFlowManager>();
+        if (gameFlowManager != null)
+        {
+            average = gameFlowManager.engagementMap.Values.Average() * 100f;
+            multiplier = updateMultiplier();
+            finalScore = average * multiplier;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.Append("Average Engagement: " + average.ToString("0.00") + "\n");
+        stringBuilder.Append("Punchline Multiplier: X" + multiplier + "\n\n");
+        stringBuilder.Append("Final Score: " + finalScore.ToString("0.00") + "\n");
+
+        gameOverDescription.text = stringBuilder.ToString();
+
+        gameStatsGameObject.SetActive(false);
+        gameOverGameObject.SetActive(true);
+
+        EventManager.RemoveListener<TimerTimeOutEvent>(OnTimerTimeoutEvent);
+    }
+
+    private float updateMultiplier()
+    {
+        float multiplier = 0.0f;
+        if (counter >= counterStep)
+        {
+            multiplier = 1.0f;
+        }
+        if (counter >= counterStep * 2)
+        {
+            multiplier = 2.0f;
+        }
+        if (counter >= counterStep * 3)
+        {
+            multiplier = 4.0f;
+        }
+        if (counter >= counterStep * 4)
+        {
+            multiplier = 8.0f;
+        }
+        if (counter >= counterStep * 4.9f)
+        {
+            multiplier = 10.0f;
+        }
+
+        return multiplier;
     }
 }
